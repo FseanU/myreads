40f3e67fca8ab7207934f7a4f26e2ee3c2a9ca14
Sean Fu <sean14xx@gmail.com>
2020-08-16 11:01:35 +0200

    feat: Create SearchPage component
    - send query to BookAPI
    - pass search result to BookList for rendering

---
+++ src/SearchPage.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+import BookList from './BookList'
+import {search} from './BooksAPI'
+
+class SearchPage extends React.Component {
+  state = {
+    input: '',
+    books: []
+  }
+
+  onHandleChange = (input) => {
+    this.setState({
+      input,
+    })
+  }
+
+  handleBooks = (query) => {
+    query && search(query)
+    .then(books=>{
+      this.setState({
+        books,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar input={this.state.input} onHandleChange={this.onHandleChange} handleBooks={this.handleBooks} />
+        <BookList books={this.state.books} />
+      </div>
+      
+    )
+  }
+}
+
+export default SearchPage
