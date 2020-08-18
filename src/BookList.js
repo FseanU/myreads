2e0d50a423543fa5cd9c77f74d84ee028d449277
Sean Fu <sean14xx@gmail.com>
2020-08-18 11:50:24 +0200

    fix: Set different id for each author

--- src/BookList.js
+++ src/BookList.js
@@ -1,41 +1,45 @@
 import React from 'react'
 
 class BookList extends React.Component {
+  onUpdateBook = (e) => {
+    console.log(e.target.value)
+    this.props.updateBook()
+  }
+
   render() { 
     console.log(this.props.books)
     return (
-      <div className="search-books-results">
-        <ol className="books-grid">
-            {(this.props.books.items && <li>No result</li>) || this.props.books.map(book=>( 
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div 
-                      className="book-cover"
-                      style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}
-                    ></div>
-                    <div className="book-shelf-changer">
-                    <select>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                    </div>
+      <ol className="books-grid">
+          {(this.props.books.items && <li>No result</li>) || this.props.books.map(book=>( 
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div 
+                    className="book-cover"
+                    style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}
+                  ></div>
+                  <div className="book-shelf-changer">
+                  <select onChange={this.onUpdateBook}>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
                   </div>
-                  <div className="book-title">{book.title}</div>
-                  {book.authors && book.authors.map(author=>(
-                    <div 
-                      className="book-authors"
-                      key={`${book.id}authors`}
-                    >{author}</div>
-                  ))}
                 </div>
-              </li>
-            ))}
-        </ol>
-      </div>
+                <div className="book-title">{book.title}</div>
+                {book.authors && book.authors.map((author, index)=>(
+                  <div 
+                    className="book-authors"
+                    key={book.id + index}
+                  >{author}</div>
+                ))}
+              </div>
+            </li>
+          ))}
+      </ol>
+      
       
     )
   }
